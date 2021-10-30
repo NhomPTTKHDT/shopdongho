99adaeab3d02f3b4e22b0fb5e335b2da690f979f
phu1204 <73231507+phu1204@users.noreply.github.com>
2021-10-30 16:31:32 +0700

    Hi
    123456

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $conn = $db->connect();
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 
 <head>
-	<title>Watch Store</title>
+	<title>Hello anh em</title>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
